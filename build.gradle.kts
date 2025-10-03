5e5464d4d829ccce1d7aef8bb01dd2c0212319bf
yosmisyael <yosmisyael7308@gmail.com>
2025-10-03 19:47:59 +0700

    init

---
+++ build.gradle.kts
@@ -0,0 +1,69 @@
+plugins {
+    java
+    application
+    id("org.javamodularity.moduleplugin") version "1.8.15"
+    id("org.openjfx.javafxplugin") version "0.0.13"
+    id("org.beryx.jlink") version "2.25.0"
+}
+
+group = "stockflow.desktop"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+val junitVersion = "5.12.1"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
+
+application {
+    mainModule.set("stockflow.desktop.stockflow")
+    mainClass.set("stockflow.desktop.stockflow.HelloApplication")
+}
+
+javafx {
+    version = "21.0.6"
+    modules = listOf("javafx.controls", "javafx.fxml", "javafx.web", "javafx.swing" , "javafx.media")
+}
+
+dependencies {
+    implementation("org.controlsfx:controlsfx:11.2.1")
+    implementation("com.dlsc.formsfx:formsfx-core:11.6.0") {
+      exclude(group = "org.openjfx")
+    }
+    implementation("net.synedra:validatorfx:0.6.1") {
+      exclude(group = "org.openjfx")
+    }
+    implementation("org.kordamp.ikonli:ikonli-javafx:12.3.1")
+    implementation("org.kordamp.bootstrapfx:bootstrapfx-core:0.4.0")
+    implementation("eu.hansolo:tilesfx:21.0.9") {
+        exclude(group = "org.openjfx")
+    }
+    implementation("com.github.almasb:fxgl:17.3") {
+        exclude(group = "org.openjfx")
+        exclude(group = "org.jetbrains.kotlin")
+    }
+    testImplementation("org.junit.jupiter:junit-jupiter-api:${junitVersion}")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:${junitVersion}")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+jlink {
+    imageZip.set(layout.buildDirectory.file("/distributions/app-${javafx.platform.classifier}.zip"))
+    options.set(listOf("--strip-debug", "--compress", "2", "--no-header-files", "--no-man-pages"))
+    launcher {
+        name = "app"
+    }
+}
