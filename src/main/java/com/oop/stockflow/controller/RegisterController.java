2e565dc3930032ec25941fd4e56f7615043abac2
yosmisyael <yosmisyael7308@gmail.com>
2025-10-07 12:25:34 +0700

    feat: build register page and fix login page

--- src/main/java/com/oop/stockflow/controller/RegisterController.java
+++ src/main/java/com/oop/stockflow/controller/RegisterController.java
@@ -1,5 +1,18 @@
 package com.oop.stockflow.controller;
 
-public class RegisterController {
+import com.oop.stockflow.app.SceneManager;
+import com.oop.stockflow.app.StageManager;
+import com.oop.stockflow.app.View;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
 
+import java.io.IOException;
+
+public class RegisterController {
+    @FXML
+    private void goToLogin(ActionEvent event) throws IOException {
+        Scene registerScene = SceneManager.load(View.LOGIN);
+        StageManager.getInstance().setScene(registerScene, "Login");
+    }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,8 @@ module com.oop.stockflow {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires javafx.base;
+    requires javafx.graphics;
 
 
     opens com.oop.stockflow to javafx.fxml;
