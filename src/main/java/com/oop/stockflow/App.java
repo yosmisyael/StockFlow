3c734e4b376b04a85a44fac39cbe658463aba852
yosmisyael <yosmisyael7308@gmail.com>
2025-10-08 00:47:44 +0700

    refactor: change main app class name

--- src/main/java/com/oop/stockflow/App.java
+++ src/main/java/com/oop/stockflow/App.java
@@ -3,15 +3,13 @@ package com.oop.stockflow;
 import com.oop.stockflow.app.SceneManager;
 import com.oop.stockflow.app.StageManager;
 import com.oop.stockflow.app.View;
-import com.oop.stockflow.db.DatabaseManager;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class MainApplication extends Application {
+public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         StageManager stageManager = StageManager.getInstance();
--- src/main/java/com/oop/stockflow/Launcher.java
+++ src/main/java/com/oop/stockflow/Launcher.java
@@ -4,6 +4,6 @@ import javafx.application.Application;
 
 public class Launcher {
     public static void main(String[] args) {
-        Application.launch(MainApplication.class, args);
+        Application.launch(App.class, args);
     }
 }
