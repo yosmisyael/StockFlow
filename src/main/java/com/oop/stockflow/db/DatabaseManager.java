7bd0353fc211df70ba471bbb4beebc6433e6209f
Afriq Fadlil <afriq.fadlil26@gmail.com>
2025-10-21 12:56:45 +0700

    feat: implement connection pooling

--- src/main/java/com/oop/stockflow/db/DatabaseManager.java
+++ src/main/java/com/oop/stockflow/db/DatabaseManager.java
@@ -1,12 +1,13 @@
 package com.oop.stockflow.db;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 import io.github.cdimascio.dotenv.Dotenv;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
 public class DatabaseManager {
-    private static Connection connection = null;
+    private static HikariDataSource dataSource;
 
     static {
         try {
@@ -16,15 +17,33 @@ public class DatabaseManager {
             String user = dotenv.get("DB_USER");
             String password = dotenv.get("DB_PASSWORD");
 
-            connection = DriverManager.getConnection(dbUrl, user, password);
+            HikariConfig config = new HikariConfig();
+            config.setJdbcUrl(dbUrl);
+            config.setUsername(user);
+            config.setPassword(password);
 
-            System.out.println("[INFO] Connected to database.");
-        } catch (SQLException e) {
+            config.setMaximumPoolSize(10);
+            config.setIdleTimeout(30000);
+            config.setLeakDetectionThreshold(20000);
+
+            dataSource = new HikariDataSource(config);
+
+            System.out.println("[INFO] Connection pool initialized.");
+
+        } catch (Exception e) {
             System.out.println("[ERROR] Unable to connect to database:  " + e.getMessage());
+            e.printStackTrace();
         }
     }
 
-    public static Connection getConnection() {
-        return connection;
+    public static Connection getConnection() throws SQLException {
+        return dataSource.getConnection();
+    }
+
+    public static void closeDataSource() throws SQLException {
+        if (dataSource != null) {
+            dataSource.close();
+            System.out.println("[INFO] Connection pool closed.");
+        }
     }
 }
