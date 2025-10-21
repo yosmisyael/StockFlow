fbb42bb4aeb3799500d2014276e81d269e7b0f82
Afriq Fadlil <afriq.fadlil26@gmail.com>
2025-10-21 12:56:13 +0700

    build: add hakari cp dep

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,6 +41,7 @@ dependencies {
 
     implementation("org.mindrot:jbcrypt:0.4")
     implementation("org.postgresql:postgresql:42.7.3")
+    implementation("com.zaxxer:HikariCP:5.1.0")
     implementation("io.github.cdimascio:java-dotenv:5.2.2")
 }
 
